9bf188b51f143d4930d20f4dfc2a615fc24fb62e
Pavel Shalnov <94354424+PashaShalnov@users.noreply.github.com>
2022-04-30 12:49:04 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,119 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+ 
+
+
+let currentQuestion = {};
+let acceptingAnswers =  false;
+let score = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(res => {
+    return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions.results.map( loadedQuestions => {
+            const formattedQuestion = {
+                question: loadedQuestions.question
+            };
+
+            const answerChoises = [...loadedQuestions.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3 ) + 1;
+            answerChoises.splice(formattedQuestion.answer -1, 0, loadedQuestions.correct_answer);
+
+            answerChoises.forEach((choice, index) => {
+                formattedQuestion["choice" + (index+1)] = choice;
+            })
+
+            return formattedQuestion
+        });
+        //questions = loadedQuestions;
+
+        startGame()
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
+//constants
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+    game.classList.remove("hidden")
+    loader.classList.add("hidden")
+};
+
+getNewQuestion = () => {
+
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+       localStorage.setItem("mostRecentScore", score)
+        return window.location.assign("/end.html");
+    }
+
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    // update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach (choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+});
+
+availableQuestions.splice(questionIndex,1);
+
+acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToAply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToAply === "correct") {
+        incrementScore(CORRECT_BONUS);
+    }
+
+    console.log(classToAply);
+
+    console.log(selectedAnswer == currentQuestion.answer);
+
+    selectedChoice.parentElement.classList.add(classToAply);
+
+    setTimeout(() => {
+        selectedChoice.parentElement.classList.remove(classToAply);
+        getNewQuestion();
+    }, 1000);
+    });
+})
+
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
+ 
